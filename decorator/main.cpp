b41a1ecbd62e4aa08b20b2c86cbecba570957d2a
Gustavo Batistela <gustavoabatistela@gmail.com>
2022-03-02 23:38:42 -0300

    Replace raw pointers by unique pointers

--- decorator/main.cpp
+++ decorator/main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <memory>
 
 // Base beverage class
 class Beverage {
@@ -47,11 +48,13 @@ public:
 // Base decorator class
 class Decorator : public Beverage {
 private:
-    Beverage *beverage;
+    std::unique_ptr<Beverage> beverage;
 public:
-    explicit Decorator(Beverage *new_beverage) : beverage{new_beverage} {}
+    explicit Decorator(std::unique_ptr<Beverage> new_beverage) : beverage{std::move(new_beverage)} {}
 
-    [[nodiscard]] Beverage *get_beverage() const { return beverage; };
+    [[nodiscard]] double get_beverage_cost() const { return beverage->cost(); };
+
+    [[nodiscard]] std::string get_beverage_description() const { return beverage->description(); };
 
     ~Decorator() override = default;
 };
@@ -59,50 +62,49 @@ public:
 // Concrete decorators
 class Mocha : public Decorator {
 public:
-    explicit Mocha(Beverage *new_beverage) : Decorator(new_beverage) {}
+    explicit Mocha(std::unique_ptr<Beverage> new_beverage) : Decorator(std::move(new_beverage)) {}
 
-    [[nodiscard]] std::string description() const override { return get_beverage()->description() + ", Mocha"; }
+    [[nodiscard]] std::string description() const override { return get_beverage_description() + ", Mocha"; }
 
-    [[nodiscard]] double cost() const override { return get_beverage()->cost() + 0.3; }
+    [[nodiscard]] double cost() const override { return get_beverage_cost() + 0.3; }
 
     ~Mocha() override = default;
 };
 
 class Soy : public Decorator {
 public:
-    explicit Soy(Beverage *new_beverage) : Decorator(new_beverage) {}
+    explicit Soy(std::unique_ptr<Beverage> new_beverage) : Decorator(std::move(new_beverage)) {}
 
-    [[nodiscard]] std::string description() const override { return get_beverage()->description() + ", Soy"; }
+    [[nodiscard]] std::string description() const override { return get_beverage_description() + ", Soy"; }
 
-    [[nodiscard]] double cost() const override { return get_beverage()->cost() + 0.5; }
+    [[nodiscard]] double cost() const override { return get_beverage_cost() + 0.5; }
 
     ~Soy() override = default;
 };
 
 class Whip : public Decorator {
 public:
-    explicit Whip(Beverage *new_beverage) : Decorator(new_beverage) {}
+    explicit Whip(std::unique_ptr<Beverage> new_beverage) : Decorator(std::move(new_beverage)) {}
 
-    [[nodiscard]] std::string description() const override { return get_beverage()->description() + ", Whip"; }
+    [[nodiscard]] std::string description() const override { return get_beverage_description() + ", Whip"; }
 
-    [[nodiscard]] double cost() const override { return get_beverage()->cost() + 0.3; }
+    [[nodiscard]] double cost() const override { return get_beverage_cost() + 0.3; }
 
     ~Whip() override = default;
 };
 
 int main() {
 
-    std::vector<Beverage *> orders;
-    orders.push_back(new Soy(new Mocha(new Espresso())));
-    orders.push_back(new Whip(new Mocha(new DarkRoast())));
-    orders.push_back(new Whip(new Soy(new HouseBlend())));
+    std::vector<std::unique_ptr<Beverage>> orders;
+    orders.push_back(std::make_unique<Soy>(std::make_unique<Mocha>(std::make_unique<HouseBlend>())));
+    orders.push_back(std::make_unique<Whip>(std::make_unique<Mocha>(std::make_unique<DarkRoast>())));
+    orders.push_back(std::make_unique<Whip>(std::make_unique<Soy>(std::make_unique<Espresso>())));
 
-    for (auto order : orders) {
+    for (const auto &order: orders) {
         static int order_num = 1;
         std::cout << "Order #" << order_num << ": " << order->description()
                   << " - $" << order->cost() << '\n';
         ++order_num;
-        delete order;
     }
 
     return 0;
